10f5b3c0cfec4f03c45caa80ed41131d10028218
HagarRam <hagaram11@gmail.com>
2022-12-18 15:55:24 +0200

    my new project

---
+++ main.js
@@ -0,0 +1,67 @@
+let someName = "Hagar";
+console.log(someName);
+someName="Ram";
+alert(someName);
+function getRandomInt(number) {
+  return Math.floor(Math.random() * max);
+}
+let number = Math.random();
+let newNumber = number*3;
+let numberString = `${number} * 3 = ${newNumber}`;
+console.log(numberString);
+
+let favoriteNumber = 5;
+let favoriteStringed = favoriteNumber.toString;
+console.log(someName);
+
+let newFavoriteNumber = 5;
+let halfOfFavNum = newFavoriteNumber/2;
+let michaelsFavorite = 13;
+console.log(michaelsFavorite - newFavoriteNumber);
+michaelsFavorite = michaelsFavorite*26;
+
+let numberOfChildren = 3;
+let partnersName = "Noa and Itay";
+let geographicLocation = "Tel Aviv";
+let jobTitle = "Having fun";
+console.log(`"You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnersName} with ${numberOfChildren} kids."
+`);
+
+let birthYear=2000;
+let futherYear=2023;
+let myFirstAge= futherYear-birthYear;
+mySecondAge= myFirstAge++;
+console.log(`I will be either ${myFirstAge} or ${mySecondAge}  in ${futherYear} ", substituting the value.`
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function greeting() {
+//   alert("welcome");
+//     console.log('hello world');
+//   }
+//   greeting();
+//   let x = 4;
+//   x+=4;
+//   console.log(x);
+//    let address = "Egoz 7 Qiryat ono";
+//    console.log(address);
+//    let myname="hagar";
+//    let mom="mommy";
+//    console.log(`${myname}love to visit ${mom} every weekend in her house. her address is ${address}`);
+//    address="bialik 20"
+//    console.log(`${myname}love to visit ${mom} every weekend in her house. her address is ${address}`);;
