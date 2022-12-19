c98b2a2325def5614e060cd85cc718108079351a
HagarRam <hagaram11@gmail.com>
2022-12-19 09:46:20 +0200

    final project

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//ex.A
 let someName = "Hagar";
 console.log(someName);
 someName="Ram";
@@ -5,21 +6,26 @@ alert(someName);
 function getRandomInt(number) {
   return Math.floor(Math.random() * max);
 }
+
+//ex.B
 let number = Math.random();
 let newNumber = number*3;
 let numberString = `${number} * 3 = ${newNumber}`;
 console.log(numberString);
 
+//ex.C
 let favoriteNumber = 5;
 let favoriteStringed = favoriteNumber.toString;
 console.log(someName);
 
+//ex.D
 let newFavoriteNumber = 5;
 let halfOfFavNum = newFavoriteNumber/2;
 let michaelsFavorite = 13;
 console.log(michaelsFavorite - newFavoriteNumber);
 michaelsFavorite = michaelsFavorite*26;
 
+//ex.E
 let numberOfChildren = 3;
 let partnersName = "Noa and Itay";
 let geographicLocation = "Tel Aviv";
@@ -27,10 +33,11 @@ let jobTitle = "Having fun";
 console.log(`"You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnersName} with ${numberOfChildren} kids."
 `);
 
+//ex.F
 let birthYear=2000;
 let futherYear=2023;
 let myFirstAge= futherYear-birthYear;
-mySecondAge= myFirstAge++;
+mySecondAge= myFirstAge--;
 console.log(`I will be either ${myFirstAge} or ${mySecondAge}  in ${futherYear} ", substituting the value.`
 );
 
